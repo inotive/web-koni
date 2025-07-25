57f04a1a03bb6896480136abe134aced5b5b9306
NoobAtProgramin <144769669+NoobAtProgramin@users.noreply.github.com>
2025-07-25 14:18:58 +0800

    fix sort_url

--- resources/views/admin/pelatih/index.blade.php
+++ resources/views/admin/pelatih/index.blade.php
@@ -3,10 +3,35 @@
 @section('pageTitle', 'Pelatih')
 @section('mainSection', 'Konfigurasi')
 @section('currentSection', 'Pelatih')
+@php
+function sortIcon($field) {
+    $currentSort = request('sort_by');
+    $currentOrder = request('order');
 
+    if ($currentSort === $field) {
+        return $currentOrder === 'asc'
+            ? '<i class="fas fa-sort-up"></i>'
+            : '<i class="fas fa-sort-down"></i>';
+    }
+
+    return '<i class="fas fa-sort text-muted"></i>';
+}
+
+function sortUrl($field) {
+    $currentSort = request('sort_by');
+    $currentOrder = request('order');
+
+    $order = ($currentSort === $field && $currentOrder === 'asc')
+        ? 'desc'
+        : 'asc';
+
+    return request()->fullUrlWithQuery([
+        'sort_by' => $field,
+        'order' => $order
+    ]);
+}
+@endphp
 @section('content')
-
-
 
 <style>
     body {
@@ -366,7 +391,7 @@
                                                 data-age="{{ $age }}"
                                                 data-prestasi="{{ $hasPrestasi }}">
 
-                                                <td></td> {{-- Will be populated by DataTable --}}
+                                                <td></td>
 
                                                 <td>
                                                     @if ($item->foto)
