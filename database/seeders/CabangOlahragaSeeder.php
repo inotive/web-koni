d907b797e63863e29848c25b72fa9c49d0db7775
ThurZ34 <fathurrahman081296@gmail.com>
2025-07-29 16:34:45 +0800

    Penambahan seeder (27 data)

--- database/seeders/CabangOlahragaSeeder.php
+++ database/seeders/CabangOlahragaSeeder.php
@@ -2,25 +2,16 @@
 
 namespace Database\Seeders;
 
-// database/seeders/CabangOlahragaSeeder.php
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\CabangOlahraga;
 
 class CabangOlahragaSeeder extends Seeder
 {
-    public function run()
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
     {
-        CabangOlahraga::create([
-            'nama_cabor' => 'Bola Basket',
-            'ketua_penanggung_jawab' => 'Andi Pratama Kusnandi',
-            'status' => 'Aktif',
-            'tanggal_pembentukan' => '2017-10-30',
-            'jumlah_atlet' => 25,
-            'jumlah_pelatih' => 3,
-            'created_at' => now(),
-        ]);
-
-        // Tambah data lainnya sesuai contoh UI...
+        //
     }
 }
-
--- database/seeders/PrestasiSeeder.php
+++ database/seeders/PrestasiSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class AtletSeeder extends Seeder
+class PrestasiSeeder extends Seeder
 {
     /**
      * Run the database seeds.
