5fee49046cc4ccc9c3c57e27b81b8b6765bae1e2
Indrayanto-20 <120290543+Indrayanto-20@users.noreply.github.com>
2025-08-07 09:50:35 +0800

    fix sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -68,7 +68,7 @@
                         <i
                             class="fa-solid {{ $isLaporanLPJActive ? 'fa-angle-up' : 'fa-angle-down' }} fs-4 {{ $isLaporanLPJActive ? 'text-orange' : 'text-gray-600' }}"></i>
                     </a>
-                    {{-- <div class="collapse {{ $isLaporanLPJActive ? 'show' : '' }}" id="submenu-laporan">
+                    <div class="collapse {{ $isLaporanLPJActive ? 'show' : '' }}" id="submenu-laporan">
                         <ul class="menu flex-column ms-5">
                             {{-- <li class="menu-item">
                                 <a class="menu-link {{ request()->is('admin/laporan-lpj/sekretariat*') ? 'active' : '' }}"
@@ -85,7 +85,7 @@
                                 </a>
                             </li>
                         </ul>
-                    </div> --}}
+                    </div>
                 </div>
 
                 <!-- Konfigurasi Section -->
