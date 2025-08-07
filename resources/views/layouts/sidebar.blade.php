8a02d0c6fca3c368f348b3959a1d0d5e52c3b2cc
Indrayanto-20 <120290543+Indrayanto-20@users.noreply.github.com>
2025-08-07 08:43:56 +0800

    fix sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -70,13 +70,13 @@
                     </a>
                     <div class="collapse {{ $isLaporanLPJActive ? 'show' : '' }}" id="submenu-laporan">
                         <ul class="menu flex-column ms-5">
-                            <li class="menu-item">
+                            {{-- <li class="menu-item">
                                 <a class="menu-link {{ request()->is('admin/laporan-lpj/sekretariat*') ? 'active' : '' }}"
                                     href="{{ route('admin.laporan-lpj.sekretariat.index') }}">
                                     <span class="menu-bullet"><span class="bullet bullet-dot"></span></span>
                                     <span class="menu-title">Sekretariat</span>
                                 </a>
-                            </li>
+                            </li> --}}
                             <li class="menu-item">
                                 <a class="menu-link {{ request()->is('admin/laporan-lpj/bidang*') ? 'active' : '' }}"
                                     href="{{ route('admin.laporan-lpj.bidang.index') }}">
