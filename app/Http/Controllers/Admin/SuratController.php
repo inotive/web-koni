680c19416b5c2141b23cf5d502aef04afbac4004
ThurZ34 <fathurrahman081296@gmail.com>
2025-08-11 10:22:21 +0800

    perbaikan bug di prestasi

---
+++ app/Http/Controllers/Admin/SuratController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Surat;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SuratController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('admin.surat.index', [
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Surat $surat)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Surat $surat)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Surat $surat)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Surat $surat)
+    {
+        //
+    }
+}
