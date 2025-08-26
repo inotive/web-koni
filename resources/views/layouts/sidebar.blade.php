99b1a9fa38c484be847553696b3cea93e74e9855
NoobAtProgramin <144769669+NoobAtProgramin@users.noreply.github.com>
2025-08-26 16:56:29 +0800

    file kesekretariat tidak tampil walau punya hak aksesnya

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -125,7 +125,7 @@
                 @endcan
 
                 <!-- File Kesekretariat - MENU BARU -->
-                @can('file-kesekretariat')
+                @can('file-kesekretariatan')
                 @php
                     $isFileKesekretariatActive =
                         request()->routeIs('admin.file-kesekretariat*') ||
